c50c17753b0c59d7b7606114bfefd7a0b9a34c95
Leif Azzopardi <leifos@acm.org>
2022-02-04 18:06:15 +0000

    Chapter 4 with completed exercises

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -8,4 +8,6 @@ def index(request):
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
+
+    #return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
+    return render(request, 'rango/about.html', context={})
