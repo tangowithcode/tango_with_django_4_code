2b86906fb5eb88b91489778a00a557f9d6a63d31
Leif Azzopardi <leifos@acm.org>
2022-11-19 15:46:03 +0000

    Chapter 11 Registration Redux

--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -30,6 +30,19 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# If True, users can register.
+REGISTRATION_OPEN = True
+	
+# If True, the user will be automatically logged in after registering.
+REGISTRATION_AUTO_LOGIN = True
+	
+# The URL that Django redirects users to after logging in.
+LOGIN_REDIRECT_URL = 'rango:index' 
+	
+# The page users are directed to if they are not logged in.
+# This was set in a previous chapter. The registration package uses this, too.
+LOGIN_URL = 'auth_login' 
+
 
 # Application definition
 
@@ -41,6 +54,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rango',
+    'registration'
 ]
 
 MIDDLEWARE = [
--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('rango/', include('rango.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('registration.backends.simple.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
